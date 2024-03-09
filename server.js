dc38b3670433c8f5a4d5654f9c7bec2490ff645b
Aman Upadhyay <104005869+amanupadhyay33822@users.noreply.github.com>
2024-03-09 23:28:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = require("./app")
 require("dotenv").config({path:"config/.env"});
 const PORT = process.env.PORT || 4000
   connect();
+app.get("/",(req,res)=>{
+  res.send("hello");
+})
 app.listen(PORT, () =>{
     console.log(`listening on ${PORT}`);
 })
